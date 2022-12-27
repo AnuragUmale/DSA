a4919576caaf268eb23acaa241ef00f82952f852
umale.anurag111@gmail.com <aumale@ucsc.edu>
2022-12-27 13:29:06 -0800

    Adding Binary Search tree with main

---
+++ Trees/binary_search_tree.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct binary_search_tree_node binary_search_tree_node;
+
+struct binary_search_tree_node{
+    int data;
+    binary_search_tree_node* left;
+    binary_search_tree_node* right;
+};
+
+
+binary_search_tree_node* create_node_for_binary_search_tree(int data){
+    binary_search_tree_node *new_node = (binary_search_tree_node*)malloc(sizeof(binary_search_tree_node));
+    if(new_node != NULL){
+        new_node->data = data;
+        new_node->left = NULL;
+        new_node->right = NULL;
+    }
+    return new_node;
+}
+
+binary_search_tree_node* insert_in_binary_search_tree(binary_search_tree_node* root,int data){
+    if(root == NULL){
+        root = create_node_for_binary_search_tree(data);
+    }
+    else if(data <= root->data){
+        root->left = insert_in_binary_search_tree(root->left,data);
+    }
+    else{
+        root->right = insert_in_binary_search_tree(root->right, data);
+    }
+    return root;
+}
+
+bool search_in_binary_search_tree(binary_search_tree_node* root, int data){
+    if(root == NULL){
+        return false;
+    }
+    else if(root->data == data){
+        return true;
+    }
+    else if(data <= root->data){
+        return search_in_binary_search_tree(root->left, data);
+    }
+    else{
+        return search_in_binary_search_tree(root->right,data);
+    }
+}
+
+int main(){
+    binary_search_tree_node* root = NULL;
+    root = insert_in_binary_search_tree(root,12);
+    root = insert_in_binary_search_tree(root,31);
+    root = insert_in_binary_search_tree(root,21);
+    root = insert_in_binary_search_tree(root,11);
+
+    if(search_in_binary_search_tree(root,21)){
+        printf("found 21\n");
+    }
+    else{
+        printf("Not found 21\n");
+    }
+
+    if(search_in_binary_search_tree(root,1)){
+        printf("found 1\n");
+    }
+    else{
+        printf("Not found 1\n");
+    }
+
+
+    return 0;
+}
