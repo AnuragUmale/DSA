f9fee75dc8b1f4640f1098fde2ef9e7778966450
umale.anurag111@gmail.com <aumale@ucsc.edu>
2022-12-29 22:15:36 -0800

    Changed code

--- Trees/level_order_traversal.c
+++ Trees/level_order_traversal.c
@@ -1,104 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-#define MAX_SIZE 1000000
-
-typedef struct binary_search_tree_node binary_search_tree_node;
-
-struct binary_search_tree_node{
-    int data;
-    binary_search_tree_node* left;
-    binary_search_tree_node* right;
+struct Node
+{
+    char data;
+    struct Node *right;
+    struct Node *left;
 };
 
-binary_search_tree_node *queue[MAX_SIZE];
+struct Node *front = NULL;
+struct Node *rear = NULL;
 
-int front = -1, rear = -1;
 
 
-void enqueue(binary_search_tree_node *address){
-    if(front == -1 && rear == -1){
-        front = rear = 0;
+void enq(struct Node *temproot)
+{
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+    temp->right = NULL;
+    temp->left = temproot;
+    temp->data = 'Z';
+
+    if (front == NULL && rear == NULL)
+    {
+        front = rear = temp;
     }
-    else if((rear + 1) % MAX_SIZE == front){
-        printf("Memory Full\n");
-        exit(1);
+    else
+    {
+        rear->right = temp;
+        rear = temp;
     }
-    else{
-        rear = (rear + 1) % MAX_SIZE;
-    }
-    queue[rear] = address;
 }
 
-void dequeue(){
-	if(front == -1 && rear == -1){
-        return;
+struct Node *deq(void)
+{
+    struct Node *temp = front;
+    if (front == NULL)
+    {
+        printf("The queue is empty!\n");
     }
-	if(front == 0 && rear == 0){
-		front = rear = -1;
-	}
-    else{
-		front = (front + 1) % MAX_SIZE;
-	}
+    else if (rear == front)
+    {
+        front = rear = NULL;
+    }
+    else
+    {
+        front = front->right;
+    }
+    struct Node *next = (temp == NULL) ? NULL : temp->left;
+    free(temp);
+    return next;
 }
 
-binary_search_tree_node *first(){
-    if(front == -1 && rear == -1){
-        printf("The queue is already empty\n");
-        return NULL;
-    }
-    return queue[front];
-}
-
-bool is_empty(){
-    if(front == -1 && rear == -1){
-        return true;
-    }
-    return false;
-}
 
 /*===============Binary Tree Section===============*/
 
-binary_search_tree_node* create_node_for_binary_search_tree(int data){
-    binary_search_tree_node *new_node = (binary_search_tree_node*)malloc(sizeof(binary_search_tree_node));
-    if(new_node != NULL){
-        new_node->data = data;
-        new_node->left = NULL;
-        new_node->right = NULL;
-    }
-    return new_node;
+struct Node *newNode(char x)
+{
+    struct Node *temp1 = (struct Node *)malloc(sizeof(struct Node));
+    temp1->data = x;
+    temp1->right = NULL;
+    temp1->left = NULL;
+    return temp1;
 }
 
-binary_search_tree_node* insert_in_binary_search_tree(binary_search_tree_node* root,int data){
-    if(root == NULL){
-        root = create_node_for_binary_search_tree(data);
+struct Node *Insert(struct Node *root, char x)
+{
+    if (root == NULL)
+    {
+        root = newNode(x);
     }
-    else if(data <= root->data){
-        root->left = insert_in_binary_search_tree(root->left,data);
+    else if (x <= root->data)
+    {
+        root->left = Insert(root->left, x);
     }
-    else{
-        root->right = insert_in_binary_search_tree(root->right, data);
+
+    else
+    {
+        root->right = Insert(root->right, x);
     }
     return root;
 }
 
-void level_order_traversal_in_binary_search_tree(binary_search_tree_node *root){
-    if(root == NULL){
-        return;
-    }
-    enqueue(root);
-    while(!is_empty()){
-        binary_search_tree_node *current = first();
-        printf("%d",current->data);
-        if(current->left != NULL){
-            enqueue(current->left);
+void LevelOrder(struct Node *root)
+{
+    struct Node *temproot = root;
+
+    while (temproot != NULL)
+    {
+        printf("%c ", temproot->data);
+        if (temproot->left != NULL)
+        {
+            enq(temproot->left);
         }
-        if(current->right != NULL){
-            enqueue(current->right);
+        if (temproot->right != NULL)
+        {
+            enq(temproot->right);
         }
-        dequeue();
+        temproot = deq();
     }
+    putchar('\n');
 }
 
 
@@ -111,12 +111,17 @@ int main(){
 			 / \   \
 			4   5   6
     */
-	binary_search_tree_node* root = NULL;
-	root = insert_in_binary_search_tree(root,1); 
-    root = insert_in_binary_search_tree(root,2);
-	root = insert_in_binary_search_tree(root,3); 
-    root = insert_in_binary_search_tree(root,4); 
-	root = insert_in_binary_search_tree(root,5); 
-    root = insert_in_binary_search_tree(root,6);
+	struct Node *root = NULL;
+    root = Insert(root, 'F');
+    root = Insert(root, 'B');
+    root = Insert(root, 'C');
+    root = Insert(root, 'D');
+    root = Insert(root, 'E');
+    root = Insert(root, 'G');
+    root = Insert(root, 'A');
+    root = Insert(root, 'H');
+    root = Insert(root, 'I');
+    LevelOrder(root);
+
     return 0;
 }
